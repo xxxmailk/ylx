d449f0d37c5173ffc4bbc14dd120ee40b3ffd12b
xxxmailk <xxxmailk@163.com>
2019-08-14 15:25:44 +0800

    update ylx strings

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# ylx
-some useful functions with golang.
+## 自己的一些有用的、常用的函数集合
+
+​		都放在这里面了，慢慢添加
+
+#### 1. 转化首字母为大写字母：
+
+```go
+// Capitalize: change first character to upper
+func Capitalize(str string) string 
+```
+
---
+++ yString.go
@@ -0,0 +1,21 @@
+package ylx
+
+// Capitalize: change first character to upper
+// 改变字符串首字母为大写
+func Capitalize(str string) string {
+	var upperStr string
+	vv := []rune(str)
+	for i := 0; i < len(vv); i++ {
+		if i == 0 {
+			if vv[i] >= 97 && vv[i] <= 122 {
+				vv[i] -= 32
+				upperStr += string(vv[i])
+			} else {
+				return str
+			}
+		} else {
+			upperStr += string(vv[i])
+		}
+	}
+	return upperStr
+}
