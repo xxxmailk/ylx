bc54a096dc757ce5aa7676ffaa5839138b7953b2
xxxmailk <xxxmailk@163.com>
2020-04-03 09:32:43 +0800

    modified:   cera/view/api_view.go

--- cera/view/api_view.go
+++ cera/view/api_view.go
@@ -2,8 +2,32 @@ package view
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"log"
 )
+
+type XmlView struct {
+	View
+}
+
+func (r *XmlView) Render() {
+	defer r.Ctx.Done()
+	rs, err := xml.Marshal(r.Data)
+	log.Println(string(rs))
+	if err != nil {
+		_, err := r.Ctx.Write([]byte(err.Error()))
+		log.Println("error: render data to xml failed, ", err)
+		return
+	}
+	// set text/xml header
+	r.Ctx.Response.Header.Set("Content-type", "text/xml")
+	_, err = r.Ctx.Write(rs)
+	if err != nil {
+		log.Println("error: write xml result to client failed,", err)
+		return
+	}
+	return
+}
 
 type JsonView struct {
 	View
